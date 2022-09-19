8ce83c929364c4bb748165134bfee55c0e71d710
bosco <boscogracia97@gmail.com>
2022-09-19 18:12:57 +0200

    Now gen_samples_txt.sh merges together reads files from the same isolate but in different pools

--- code/gen_samples_txt.sh
+++ code/gen_samples_txt.sh
@@ -2,9 +2,9 @@
 
 cd ~/PhD
 
-echo 'Which bacteria do you want to analyse? Type it in the terminal with the format: Genus_species'
+echo 'Which bacteria do you want to analyse? Type it in the terminal with the format: "Genus_species"'
 read
-
+REPLY=Acetobacter_malorum
 SPECIES=$(echo $REPLY | sed 's/_/ /')
 
 mkdir Microbiome_pangenomic_analysis/data/$REPLY
@@ -21,9 +21,23 @@ SAMPLES=($(awk -v s="$SPECIES" -F "\t" '$2 ~ s {print $1}' $TEMP/taxonomy.tsv | 
 echo -e 'sample\tr1\tr2' > $TEMP/header.tmp
 touch $TEMP/body.tmp
 for i in ${SAMPLES[*]};
-do awk -F "," -v i="$i" '$2==i {print $2,"Isolates_assembly/Pool_"substr($2,1,3)"/02.Rm_adapters/fastq/"$1"_1.fq.gz","Isolates_assembly/Pool_"substr($2,1,3)"/02.Rm_adapters/fastq/"$1"_2.fq.gz"}' $TEMP/names.csv >> $TEMP/body.tmp;
+do  awk -F "," -v i="$i" '$2==i {print $2,"Isolates_assembly/Pool_"substr($2,1,3)"/02.Rm_adapters/fastq/"$1"_1.fq.gz","Isolates_assembly/Pool_"substr($2,1,3)"/02.Rm_adapters/fastq/"$1"_2.fq.gz"}' $TEMP/names.csv >> $TEMP/body.tmp;
 done
+cut -d "_" -f2- $TEMP/body.tmp | sort | awk 'FNR==1{
+              print
+              next
+            }
+            {
+              A[$1]=$1 in A ? A[$1]","$2:$2
+            }
+            {
+              B[$1]=$1 in B ? B[$1]","$3:$3
+            }
+         END{
+              for(i in A)
+                   print i,A[i],B[i]
+            }' >> body_merged.tmp # This strange awk command that I don't fully understand merges together rows by sample name and merges together the location of the r1 and r2 with commas.
 
-cat $TEMP/header.tmp $TEMP/body.tmp > $WORKDIR/samples-txt
+cat $TEMP/header.tmp $TEMP/body_merged.tmp > $WORKDIR/samples-txt
 
 rm -r $TEMP
