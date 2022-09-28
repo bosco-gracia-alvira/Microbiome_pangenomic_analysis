d5b69576d0cee795a6e361c418379edcd97b754f
bosco <boscogracia97@gmail.com>
2022-09-28 17:22:10 +0200

    The script uses the RefSeq representative genome as scaffold reference.

--- code/gen_fasta_txt.sh
+++ code/gen_fasta_txt.sh
@@ -22,20 +22,15 @@ TEMP=Microbiome_pangenomic_analysis/data/temp
 cat Isolates_assembly/Pool_???/07.GTDB-Tk/summary.tsv > $TEMP/taxonomy.tsv
 SAMPLES=$(awk -v s="$SPECIES" -F "\t" '$2 ~ s {print $1}' $TEMP/taxonomy.tsv | sort)
 
-SPECIES="Lactiplantibacillus plantarum"
-LEVELS=(complete chromosome scaffold)
-n=1
-while [[ $(basename -a temp/*.fna.gz | wc -l)==0 || $n -lt 5 ]]
-do  ncbi-genome-download bacteria \
+ncbi-genome-download bacteria \
         -g "$SPECIES" \
         -s refseq \
+        -R "reference,representative" \
         -F fasta \
-        -l $LEVELS[$n]\
+        -l all \
         -P \
         --flat-output \
-        -o temp
-    ((n++))
-done
+        -o $TEMP
 
 gunzip $TEMP/*.gz
 
@@ -47,28 +42,32 @@ read ISOLATE
 
 cp Isolates_assembly/Pool_$(echo $ISOLATE | cut -f1 -d "_")/07.GTDB-Tk/Genomes/$ISOLATE.fa $TEMP
 
-if [[ $(basename -a $TEMP/*.fna | wc -l) == 1 ]]
-then
-    ragtag.py scaffold -o $TEMP/Scaffold $TEMP/*.fna $TEMP/$ISOLATE.fa;
-    cp $TEMP/Scaffold/*.fasta $WORKDIR/reference.fa
-
-elif [[ $(basename -a $TEMP/*.fna | wc -l) -gt 1 ]]
-then
-    declare -i x=1;
-    for i in $(basename -a $TEMP/*.fna);
-        do  ragtag.py scaffold -o $TEMP/multiple_$x $TEMP/$i $TEMP/$ISOLATE.fa;
-            x=$x+1;
-    done;
-    ragtag.py merge $TEMP/$ISOLATE.fa $TEMP/multiple_*/*.agp -o $TEMP/final_multiple;
-    cp $TEMP/final_multiple/ragtag.merge.fasta $WORKDIR/reference.fa
-elif [[ $(basename -a $TEMP/*.fna | wc -l) == 0 ]]
-then
-    echo "I could not download any reference genome from NCBI, therefore the un-scaffolded assembly will be used as reference";
-    cp $TEMP/$ISOLATE.fa $WORKDIR/reference.fa
-fi
+ragtag.py scaffold -o $TEMP/Scaffold $TEMP/*.fna $TEMP/$ISOLATE.fa;
+cp $TEMP/Scaffold/*.fasta $WORKDIR/reference.fa
 
 echo -e "name\tpath\n$(echo $ISOLATE | cut -f2- -d "_")\t$WORKDIR/reference.fa" > $WORKDIR/fasta-txt
 
 rm -r $TEMP
 
 assembly-stats $WORKDIR/reference.fa
+
+#This part of the script is under maintainance. In theory, it behaves different if you scaffold based on just one reference genome or based on several. But as I use the 
+#if [[ $(basename -a $TEMP/*.fna | wc -l) == 1 ]]
+#then
+#    ragtag.py scaffold -o $TEMP/Scaffold $TEMP/*.fna $TEMP/$ISOLATE.fa;
+#    cp $TEMP/Scaffold/*.fasta $WORKDIR/reference.fa
+#
+#elif [[ $(basename -a $TEMP/*.fna | wc -l) -gt 1 ]]
+#then
+#    declare -i x=1;
+#    for i in $(basename -a $TEMP/*.fna);
+#        do  ragtag.py scaffold -o $TEMP/multiple_$x $TEMP/$i $TEMP/$ISOLATE.fa;
+#            x=$x+1;
+#    done;
+#    ragtag.py merge $TEMP/$ISOLATE.fa $TEMP/multiple_*/*.agp -o $TEMP/final_multiple;
+#    cp $TEMP/final_multiple/ragtag.merge.fasta $WORKDIR/reference.fa
+#elif [[ $(basename -a $TEMP/*.fna | wc -l) == 0 ]]
+#then
+#    echo "I could not download any reference genome from NCBI, therefore the un-scaffolded assembly will be used as reference";
+#    cp $TEMP/$ISOLATE.fa $WORKDIR/reference.fa
+#fi
