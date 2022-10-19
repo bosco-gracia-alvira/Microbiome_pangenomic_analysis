ecc9cda84f12052a99df7323df2138e59572754d
bosco <boscogracia97@gmail.com>
2022-10-19 18:04:15 +0200

    Changed the working directory

--- code/Anvio_display.sh
+++ code/Anvio_display.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd ~/PhD/data/Microbiome_pangenomic_analysis/data
-
 echo 'Which Anvio profile do you want to display? Type in the terminal the species with the format: "Genus_species"'
 read
 
-CONTIG=$(cat $REPLY/fasta-txt | awk '!/name/ {print $1}')
+cd ~/PhD/Microbiome_pangenomic_analysis/data/$REPLY/Anvio
+
+CONTIG=$(cat fasta-txt | awk '!/name/ {print $1}')
 
 eval "$(conda shell.bash hook)"
 conda activate anvio-7.1
 
-anvi-interactive -c $REPLY/Anvio/03_CONTIGS/$CONTIG-contigs.db \
-                 -p $REPLY/Anvio/06_MERGED/$CONTIG/PROFILE.db
+anvi-interactive -c 03_CONTIGS/$CONTIG-contigs.db \
+                 -p 06_MERGED/$CONTIG/PROFILE.db
