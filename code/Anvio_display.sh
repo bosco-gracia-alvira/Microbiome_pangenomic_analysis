9da9c7c043c3fc33983ee0bad7fc7648d794bc38
bosco <boscogracia97@gmail.com>
2022-10-19 15:22:15 +0200

    Changed a typo

--- code/Anvio_display.sh
+++ code/Anvio_display.sh
@@ -2,7 +2,7 @@
 
 cd ~/PhD/data/Microbiome_pangenomic_analysis/data
 
-echo 'Which Anvio profile do you want to annotate? Type in the terminal the species with the format: "Genus_species"'
+echo 'Which Anvio profile do you want to display? Type in the terminal the species with the format: "Genus_species"'
 read
 
 CONTIG=$(cat $REPLY/fasta-txt | awk '!/name/ {print $1}')
