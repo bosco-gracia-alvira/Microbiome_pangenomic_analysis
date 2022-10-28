8a1bfec1e16aee9c6b4ce8f26094461c426429da
bosco <boscogracia97@gmail.com>
2022-10-28 13:26:20 +0200

    It works now

--- code/Anvio_pangen_wf.sh
+++ code/Anvio_pangen_wf.sh
@@ -1,19 +1,23 @@
-https://merenlab.org/2018/07/09/anvio-snakemake-workflows/#pangenomics-workflow
+#!/bin/bash
 
+cd ~/PhD
+
+echo 'Which species do you want to analyse with Anvio? Type in the terminal the species with the format: "Genus_species"'
+echo
+cat Isolates_assembly/Pool_???/07.GTDB-Tk/summary.tsv | cut -f2 | rev | cut -d "_" -f1 | rev | grep " "| sed 's/ /_/' | sort | uniq -c | sort -r | column
+read
+
+if [[ ! -d ~/PhD/Microbiome_pangenomic_analysis/data/$REPLY/Anvio_pangen ]]
+then
+        echo -e "The species $REPLY is not availabe :(";
+        echo -e "Maybe you have forgotten to run gen_pangen_fasta_txt.sh"
+        exit
+fi
+
+cd ~/PhD/Microbiome_pangenomic_analysis/data/$REPLY/Anvio_pangen
+
+eval "$(conda shell.bash hook)"
+conda activate anvio-7.1
 
 anvi-run-workflow -w pangenomics \
-                  -c config-pangenomics.json \
-                  --save-workflow-graph
-
-{
-    "workflow_name": "pangenomics",
-    "config_version": "2",
-    "project_name": "MYPAN",
-    "external_genomes": "my-external-genomes.txt",
-    "fasta_txt": "fasta.txt",
-    "output_dirs": {
-        "FASTA_DIR": "01_FASTA_contigs_workflow",
-        "CONTIGS_DIR": "02_CONTIGS_contigs_workflow",
-        "LOGS_DIR": "00_LOGS_pan_workflow"
-    }
-}
+                  -c ../../../code/config-pangen.json
