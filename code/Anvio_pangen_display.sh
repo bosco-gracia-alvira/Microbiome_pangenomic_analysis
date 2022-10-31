d0e2c33cb841f3c0856fc62b4e2e804eac68df3f
bosco <boscogracia97@gmail.com>
2022-10-31 11:50:01 +0100

    Made Anvio_display for each wf

---
+++ code/Anvio_pangen_display.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Which Anvio profile do you want to display? Type in the terminal the species with the format: "Genus_species"'
+read
+
+cd ~/PhD/Microbiome_pangenomic_analysis/data/$REPLY/Anvio_pangen
+
+CONTIG=$(cat fasta-txt | awk '!/name/ {print $1}')
+
+eval "$(conda shell.bash hook)"
+conda activate anvio-7.1
+
+anvi-display-pan -g 03_PAN/MYPAN-GENOMES.db \
+                 -p 03_PAN/MYPAN-PAN.db
