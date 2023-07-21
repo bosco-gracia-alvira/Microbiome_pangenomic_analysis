8f86738f49987a25f1b5a34deedc3014d166899e
bosco <boscogracia97@gmail.com>
2023-07-21 14:50:04 +0200

    Included taxon-specific enrichment

--- code/Anvio_pangen_enrichment.sh
+++ code/Anvio_pangen_enrichment.sh
@@ -12,14 +12,14 @@ conda activate anvio-7.1
 
 cd "$START" || exit
 
-echo 'To which species do you want to generate a misc-table? Type in the terminal the species with the format: "Genus_species"'
+echo 'To which taxon do you want to generate a misc-table? Type in the terminal the species with the format "Genus_species" or the genus with the format "Genus"'
 echo
 cat "$START"/Isolates_assembly/Pool_???/07.GTDB-Tk/summary.tsv | cut -f2 | rev | cut -d "_" -f1 | rev | grep " "| sed 's/ /_/' | sort | uniq -c | sort -k2 | column
 read
 
-WORKDIR="$START"/Microbiome_pangenomic_analysis/data/$REPLY/Anvio_pangen
+PANGEN="$START"/Microbiome_pangenomic_analysis/data/$REPLY/Anvio_pangen
 
-if [[ ! -d "$WORKDIR"/03_PAN ]]
+if [[ ! -d "$PANGEN"/03_PAN ]]
 then    
         echo
         echo
@@ -28,7 +28,31 @@ then
         echo
         exit
 else
-        mkdir -p "$WORKDIR"/05_ENRICHMENT
+        mkdir -p "$PANGEN"/05_ENRICHMENT
+fi
+
+
+# This chunk of code only works if the user is working with a whole genus.
+if [[ ! $REPLY =~ "_" ]]
+then
+        cat "$START"/Isolates_assembly/Pool_???/07.GTDB-Tk/summary.tsv | grep "${REPLY}" | cut -f1  | cut -d "_" -f2- > "$PANGEN"/Genomes.tmp
+        cat "$START"/Isolates_assembly/Pool_???/07.GTDB-Tk/summary.tsv | grep "${REPLY}" | cut -f2 | rev | cut -d "_" -f1 | rev | grep " " | sed 's/ /_/' > "$PANGEN"/Clade.tmp
+
+        paste "$PANGEN"/Genomes.tmp "$PANGEN"/Clade.tmp > "$PANGEN"/Bins_to_taxonomy.tmp
+        echo -e "name\tspecies" > "$PANGEN"/header.tmp
+        cat "$PANGEN"/header.tmp "$PANGEN"/Bins_to_taxonomy.tmp > "$PANGEN"/Bins_to_taxonomy.txt
+
+        sed "s/species/group/" "$PANGEN"/Bins_to_taxonomy.txt > "$PANGEN"/Enrichment.tmp
+
+        for i in {COG20_FUNCTION,COG20_CATEGORY,KOfam,KEGG_Module,COG20_PATHWAY,KEGG_Class}
+        do      anvi-compute-functional-enrichment-across-genomes -e "$PANGEN"/my-external-genomes.txt \
+                                                        -o "$PANGEN"/05_ENRICHMENT/Taxon_enrichment"${i}".txt \
+                                                        -G "$PANGEN"/Enrichment.tmp \
+                                                        --annotation-source "${i}"
+        done
+
+        rm "$PANGEN"/*.tmp
+
 fi
 
 
