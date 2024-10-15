bfaf40edb1763ea10961c30b62708377e2337717
bosco <boscogracia97@gmail.com>
2024-10-15 16:30:11 +0200

    Removed all the downloading commands

--- code/Lac_phylogeny.sh
+++ code/Lac_phylogeny.sh
@@ -7,11 +7,7 @@
 # Set the paths
 WORKDIR="/Users/bgracia/PopGen Dropbox/Martin McFly/Bosco/PhD_Dropbox/Microbiome_pangenomic_analysis/data/Lactiplantibacillus_plantarum/Phylogeny"
 ISOLATES="/Users/bgracia/PopGen Dropbox/Martin McFly/Bosco/PhD_Dropbox/Microbiome_pangenomic_analysis/data/Isolates"
-URL_FASTA="https://www.ebi.ac.uk/ena/browser/api/fasta"
-SCRIPTS="$WORKDIR/ENABrowserTools/python3"
 FASTAS="$WORKDIR/fastas"
-URL_METADATA="https://www.ebi.ac.uk/ena/browser/api/xml"
-METADATA="$WORKDIR/metadata"
 
 ### COMMANDS
 IFS=$'\n'
@@ -23,25 +19,7 @@ then
   exit
 fi
 
-if [[ ! -d "$SCRIPTS" ]]
-then
-  echo "You need the enaBrowserTools scripts. Get them from:
-  https://github.com/enasequence/enaBrowserTools?tab=readme-ov-file"
-  exit
-fi
-
 # Create the subfolders if they don't exist
 mkdir -p "$FASTAS"
 mkdir -p "$METADATA"
 
- 
-python3 "$SCRIPTS"/enaDataGet.py -f fasta -d "$FASTAS" GCA_000203855
-
-
-# Loop through each accession number and download the corresponding files
-while IFS=$'\t' read -r strain accession; do
-    echo "Downloading sequence data for $strain..."
-    python3 "$SCRIPTS"/enaDataGet.py -m -f fasta -d "$FASTAS" "${accession}"
-done < <(cut -f1,8 "$WORKDIR"/metadata.tsv | grep -v "Strain")
-
-echo "Download completed."
