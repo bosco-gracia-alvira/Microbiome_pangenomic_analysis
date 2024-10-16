f893babc5c41b9e52a302013a15757d512e584a4
bosco <boscogracia97@gmail.com>
2024-10-16 12:17:22 +0200

    Make whole phylogeny with GTDB

--- code/Lac_phylogeny.sh
+++ code/Lac_phylogeny.sh
@@ -6,20 +6,84 @@
 
 # Set the paths
 WORKDIR="/Users/bgracia/PopGen Dropbox/Martin McFly/Bosco/PhD_Dropbox/Microbiome_pangenomic_analysis/data/Lactiplantibacillus_plantarum/Phylogeny"
-ISOLATES="/Users/bgracia/PopGen Dropbox/Martin McFly/Bosco/PhD_Dropbox/Microbiome_pangenomic_analysis/data/Isolates"
-FASTAS="$WORKDIR/fastas"
+FASTAS="$WORKDIR/ENAfastas"
+METADATA="$WORKDIR/metadata.tsv"
 
 ### COMMANDS
 IFS=$'\n'
 
-# Check that you have the accessions file and the scripts
-if [[ ! -f "$WORKDIR"/accessions.txt ]]
+# Check that you have the metadata file
+if [[ ! -f "$WORKDIR/metadata.tsv" ]]
 then
-  echo "You need the list with accessions of publicly available genomes delimited by newlines"
+  echo "You need a metadata table with isolates"
   exit
 fi
 
-# Create the subfolders if they don't exist
-mkdir -p "$FASTAS"
-mkdir -p "$METADATA"
+# Loop through each accession number and download the corresponding files
+# while IFS=$'\t' read -r strain accession
+# do
+#     cp "$FASTAS"/"${accession%0000000}"1.fasta "$FASTAS"/$strain.fa
+# done < <(cut -f1,8 "$WORKDIR"/metadata.tsv | grep -v "Strain" | grep -v "GCA_" | sed "s/ //g")
 
+# while IFS=$'\t' read -r strain accession
+# do
+#     cp "$FASTAS"/"${accession}".fasta "$FASTAS"/$strain.fa
+# done < <(cut -f1,8 "$WORKDIR"/metadata.tsv | grep -v "Strain" | grep "GCA_" | sed "s/ //g")
+
+ssh -T vetlinux05@pgnsrv043.vu-wien.ac.at << FOO
+rm -r ~/Bosco/Microbial_pangenomic_analysis/Lactiplantibacillus_plantarum/Phylogeny
+mkdir -p \
+    ~/Bosco/Microbial_pangenomic_analysis/Lactiplantibacillus_plantarum/Phylogeny/{genomes,identity,alignment,tree}
+
+FOO
+
+# Copy the ENA genomes to the server
+rsync -av \
+    "$FASTAS"/*.fa \
+    vetlinux05@pgnsrv043.vu-wien.ac.at:~/Bosco/Microbial_pangenomic_analysis/Lactiplantibacillus_plantarum/Phylogeny/genomes
+
+
+# Copy my genomes to the server
+cd "$WORKDIR" || exit
+
+while IFS=$'\t' read -r path
+do
+    rsync -av \
+        "$path" \
+        vetlinux05@pgnsrv043.vu-wien.ac.at:~/Bosco/Microbial_pangenomic_analysis/Lactiplantibacillus_plantarum/Phylogeny/genomes/$(basename ${path%sta})
+done < <(cut -f2 "$WORKDIR"/Isolates_path | grep -v "path")
+
+# We connect to the server again to run the first steps of GTDB-Tk
+ssh -T vetlinux05@pgnsrv043.vu-wien.ac.at << FOO
+
+cd ~/Bosco/Microbial_pangenomic_analysis/Lactiplantibacillus_plantarum/Phylogeny
+
+eval \$(conda shell.bash hook)
+conda activate gtdbtk-2.1.1
+
+export GTDBTK_DATA_PATH="/home/vetlinux05/Bosco/db/gtdbtk_r214_database"
+
+# Identify the 
+gtdbtk identify \
+    -x fa \
+    --genome_dir genomes \
+    --out_dir identity \
+    --cpus 20
+
+gtdbtk align \
+    --taxa_filter "g__Lactiplantibacillus" \
+    --identify_dir identity \
+    --out_dir alignment \
+    --cpus 20
+
+gtdbtk infer \
+    --msa_file alignment/align/gtdbtk.bac120.msa.fasta.gz \
+    --out_dir tree \
+    --cpus 20
+
+FOO
+
+# I copy the results and the genomes back to my computer
+rsync -av \
+    vetlinux05@pgnsrv043.vu-wien.ac.at:~/Bosco/Microbial_pangenomic_analysis/Lactiplantibacillus_plantarum/Phylogeny/* \
+    "$WORKDIR"
